8ce254cd3c70b00914490d3fb82ac686c466d389
myawka <ssscolllo@gmail.com>
2024-10-17 20:30:21 +0300

    basics

---
+++ basics/tasks 18-20/script.js
@@ -0,0 +1,31 @@
+// 18_1
+let a1 = 10;
+let b1 = 2;
+
+alert(a1+b1)
+alert(a1-b1)
+alert(a1*b1)
+alert(a1/b1)
+
+// 18_2
+let c = 10;
+let d = 5;
+let result= c + d;
+
+alert(result)
+
+// 18_3
+let a3 = 1;
+let b3 = 2;
+let c3 = 3;
+
+alert(a3+b3+c3)
+
+// 18_4
+let a4 = 10;
+let b4 = 5;
+let c4 = a4 - b4;
+let d4 = 7;
+let result1 = c4 + d4;
+
+alert(result1);
