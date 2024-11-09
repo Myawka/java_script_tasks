1b571d09145937e1c6a7109765d094ac4979c0b9
myawka <ssscolllo@gmail.com>
2024-11-09 11:06:15 +0300

    arrays

--- arrays/tasks 75-79/script.js
+++ arrays/tasks 75-79/script.js
@@ -27,3 +27,25 @@ let arr77_2 = [1, 2, 3, 4, 5];
 let key77_2 = 1;
 let key77_3 = 2;
 console.log(arr77_2[key77_2] + arr77_2[key77_3]);
+
+//78_1
+let arr78 = ['a', 'b', 'c', 'd', 'e']
+delete arr78[0];
+delete arr78[1];
+console.log(arr78.length)
+
+//79_1
+let arr79_1 = [1, 2, 3, 4, 5];
+console.log(arr79_1[arr79_1.length-1]);
+
+//79_2
+let arr79_2 = [1, 2, 3, 4, 5];
+console.log((arr79_2[1]) + (arr79_2[2]) +(arr79_2[3]) + (arr79_2[4]) + arr79_2[0]);
+
+//79_3
+let arr79_3 = [1, 2, 3, 4, 5];
+console.log(arr79_3.length);
+
+//79_4
+let arr79_4 = [1, 2, 3, 4, 5];
+console.log(arr79_4.length);
