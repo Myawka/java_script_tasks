7bd833a60655292a557bba32afab2c1ad88f4f7e
myawka <ssscolllo@gmail.com>
2024-11-09 11:05:05 +0300

    arrays

---
+++ arrays/tasks 75-79/script.js
@@ -0,0 +1,29 @@
+//75_1
+let arr75 = [];
+arr75[3] = 'a';
+arr75[8] = 'b';
+console.log(arr75.length);
+
+//76_1
+let arr76_1 = [];
+arr76_1.push(1);
+arr76_1.push(2);
+arr76_1.push(3);
+console.log(arr76_1)
+
+//76_2
+let arr76_2 = [1, 2, 3];
+arr76_2.push(4);
+arr76_2.push(5);
+console.log(arr76_2)
+
+//77_1
+let arr77_1 = ['a', 'b', 'c'];
+let key77_1 = 2;
+console.log(arr77_1[key77_1]);
+
+//77_2
+let arr77_2 = [1, 2, 3, 4, 5];
+let key77_2 = 1;
+let key77_3 = 2;
+console.log(arr77_2[key77_2] + arr77_2[key77_3]);
