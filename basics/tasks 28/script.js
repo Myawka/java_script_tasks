241fc90a07eddc4b281f950a27e7f2a1b519baf9
myawka <ssscolllo@gmail.com>
2024-11-09 08:45:57 +0300

    basics

--- basics/tasks 28/script.js
+++ basics/tasks 28/script.js
@@ -4,3 +4,16 @@ alert(name);
 
 let surname = "Ablyazizova";
 alert(surname);
+
+//29_1
+let str = "!!!";
+alert(str);
+
+//29_2
+let a = "java";
+let b = "script";
+alert(a+b);
+
+//29_3
+let c = "hello";
+let d = "world";
