fd99611fe42dd079c32c0930b0f92125dbe7649f
myawka <ssscolllo@gmail.com>
2024-11-09 10:28:28 +0300

    basics

---
+++ basics/tasks 51-67/script.js
@@ -0,0 +1,123 @@
+//51_1
+// let test = Boolean(3);
+// alert(test)
+// Результат = true
+
+//51_2
+// let test = Boolean(0);
+// alert(test)
+// Результат = false
+
+//51_3
+// let test = Boolean(-1);
+// alert(test)
+// Результат = true
+
+//51_4
+// let test = Boolean(-0);
+// alert(test)
+// Результат = false
+
+//51_5
+// let test = Boolean(+0);
+// alert(test)
+// Результат в= false
+
+//51_6
+// let test = Boolean('abc');
+// alert(test)
+// Результат = true
+
+//51_7
+// let test = Boolean('');
+// alert(test)
+// Результат = false
+
+//51_8
+// let test = Boolean('0');
+// alert(test)
+// Результат = true
+
+//51_9
+// let test = Boolean(true);
+// alert(test)
+// Результат = true
+
+//51_0
+// let test = Boolean(false);
+// alert(test)
+// Результат = false
+
+//51_11
+// let test = Boolean('true');
+// alert(test)
+// Результат = true
+
+//51_12
+// let test = Boolean('false');
+// alert(test)
+// Результат = true
+
+//51_13
+// let test = Boolean(null);
+// alert(test)
+// Результат = false
+
+//51_14
+// let test = Boolean('null');
+// alert(test)
+// Результат = true
+
+//51_15
+// let test = Boolean(undefined);
+// alert(test)
+// Результат = false
+
+//51_16
+// let test = Boolean('undefined');
+// alert(test)
+// Результат = true
+
+//51_17
+// let test = Boolean(NaN);
+// alert(test);
+// Результат = false
+
+//51_18
+// let test = Boolean('NaN');
+// alert(test);
+// Результат  = true
+
+//51_19
+// let test = Boolean(3 * 'abc');
+// alert(test);
+// Результат  = false
+
+//51_20
+// let test = Boolean(Infinity);
+// alert(test); 
+// Результат  = true
+
+//51_21
+//let test = Boolean(1 / 0);
+// alert(test);
+// Результат = true
+
+//52_1
+let str52_1 = 'abcde';
+alert(str52_1[0]); 
+alert(str52_1[2]); 
+alert(str52_1[4]); 
+
+//52_2
+let str52_2 = 'abcde';
+alert(str52_2[4]); 
+alert(str52_2[3]); 
+alert(str52_2[2]); 
+alert(str52_2[1]); 
+alert(str52_2[0]);
+
+//52_3
+let str52_3 = 'abcde';
+let num = 3; 
+alert(str52_3[num]);
