16963c38f79dea78ad386d1e1acc8918238a71b9
myawka <ssscolllo@gmail.com>
2024-11-09 10:58:58 +0300

    arrays

--- arrays/tasks 68-103/script.js
+++
@@ -1,9 +0,0 @@
-//68_1
-let arr68_1 = [1, 2, 3]; 
-alert(arr68_1);
-console.log(arr68_1);
-
-//68_2
-let arr68_2 = ["myawka123"]; 
-alert(arr68_2);
-console.log(arr68_2);
---
+++ arrays/tasks 68-74/script.js
@@ -0,0 +1,29 @@
+//68_1
+let arr68_1 = [1, 2, 3]; 
+alert(arr68_1);
+console.log(arr68_1);
+
+//68_2
+let arr68_2 = ["myawka123"]; 
+alert(arr68_2);
+console.log(arr68_2);
+
+//69_1
+let arr69_1 = [1, 2, 3]; 
+console.log(arr69_1[0]); 
+console.log(arr69_1[1]); 
+console.log(arr69_1[2]);
+
+//69_2
+let arr69_2 = [1, 2, 3]; 
+console.log(arr69_2[0]); 
+console.log(arr69_2[1]); 
+console.log(arr69_2[2]);
+
+//69_3
+let arr69_3 = [1, 2, 3];
+console.log(arr69_3[0] + arr69_3[1] + arr69_3[2]);
+
+//69_4
+let arr69_4 = ['a', 'b', 'c', 'd'];
+console.log(String(arr69_4[0])+ "+" + String(arr69_4[1])+ '+' + String(arr69_4[2])+ '+' + String(arr69_4[3]));
