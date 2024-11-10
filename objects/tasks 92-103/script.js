e9f08357883bb69194b04927763f171fef9f7397
myawka <ssscolllo@gmail.com>
2024-11-10 22:37:18 +0300

    objects

---
+++ objects/tasks 92-103/script.js
@@ -0,0 +1,10 @@
+//92_1
+let obj92_1 = {x: 1, y: 2, z: 3};
+let prop92_1 = 'x';
+console.log(obj92_1[prop92_1]);
+
+//92_2
+let obj92_2 = {x: 1, y: 2, z: 3};
+
+let prop92_2 = 'x';
+console.log(obj92_2[prop92_2]);
