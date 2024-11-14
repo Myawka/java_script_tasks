fa1d2aa05d79ded1c2c8bbc9fa2c3cb09b551b5f
myawka <ssscolllo@gmail.com>
2024-11-14 09:54:50 +0300

    conditions

---
+++ conditions/script.js
@@ -0,0 +1,31 @@
+//105_1
+let test105_1 = 20;
+if (test105_1 > 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+//105_2
+let test105_2 = 5;
+if (test105_2 < 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+//105_3
+let test105_3 = 10;
+if (test105_3 >= 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
+
+//105_4
+let test105_4 = 2;
+if (test105_4 <= 10) {
+	console.log('+++'); 
+} else {
+	console.log('---');
+}
