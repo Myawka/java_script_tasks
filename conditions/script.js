ad833fd41a229c0cb01d9301bf486b4a4814fa6d
myawka <ssscolllo@gmail.com>
2024-11-17 21:52:33 +0300

    conditions

--- conditions/script.js
+++ conditions/script.js
@@ -209,3 +209,81 @@ if (num113_3 <= 1 && num113_4 >= 3) {
 } else {
 	console.log('---');
 }
+
+//114_1
+// let num1 = -10;
+// let num2 = -10;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Вывод = ---
+ 
+//114_2
+// let num1 = 0;
+// let num2 = 0;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Вывод = +++
+        
+//114_3
+// let num1 = 0;
+// let num2 = 5;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Вывод = +++
+
+//114_4
+// let num1 = 5;
+// let num2 = 5;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Вывод = +++
+
+//114_5
+// let num1 = -5;
+// let num2 = 15;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Вывод = ---
+
+//114_6
+// let num1 = -5;
+// let num2 = 15;
+// if (num1 >= 0 && num2 >= 0) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Вывод = ---
+
+//114_7
+// let num = 2;
+// if (num == 0 || num == 1) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Вывод = ---
+
+//114_8
+// let num = 2;
+// if (num == 0 || num == 1 || num == 2) {
+//   console.log('+++');
+// } else {
+//   console.log('---');
+// }
+// Вывод = +++
