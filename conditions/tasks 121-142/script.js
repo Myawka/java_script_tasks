576966acab00ca3ea47f83ba69a2285e6b237778
myawka <ssscolllo@gmail.com>
2024-11-23 19:19:40 +0300

    conditions

---
+++ conditions/tasks 121-142/script.js
@@ -0,0 +1,23 @@
+//121_1
+let test121_1 = true;
+if (!test121_1) {
+  console.log('+++');
+} else {
+  console.log('---');
+}
+
+//121_2
+let test121_2 = true;
+if (!test121_2) {
+  console.log('+++');
+} else {
+  console.log('---');
+}
+
+//121_3
+let test121_3 = true;
+if (test121_3) {
+  console.log('+++');
+} else {
+  console.log('---');
+}
