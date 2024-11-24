95d4b8061cbe03d2f815cc45d6e14a62f35e71c7
myawka <ssscolllo@gmail.com>
2024-11-24 20:07:06 +0300

    loops

--- loops/script.js
+++ loops/script.js
@@ -1,6 +1,20 @@
 //144_1
-console.log('Вывод элементов массива:');
+console.log('Вывод элементов массива 144:');
 let arr144 = ['a', 'b', 'c', 'd', 'e'];
 for (let elem144 of arr144) {
 	console.log(elem144);
+}
+
+// Задание 145 №1
+let obj145_1 = {x: 1, y: 2, z: 3};
+console.log('Ключи объекта 145_1:'); 
+for (let key145_1 in obj145_1) {
+	console.log(key145_1); 
+}
+
+// Задание 145 №2
+let obj145_2 = {x: 1, y: 2, z: 3};
+console.log('Элементы объекта 145_2:'); 
+for (let key145_2 in obj145_2) {
+	console.log(obj145_2[key145_2]);
 }
