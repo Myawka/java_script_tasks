6fe5769e7c9ed687f8f8378af15486c4d3edf380
myawka <ssscolllo@gmail.com>
2024-12-02 19:31:06 +0300

    loops

---
+++ loops/task 167/script.js
@@ -0,0 +1,34 @@
+//167_1
+console.log('Результат задания 167 №1:');
+for (let i = 1; i <= 100; i++) {
+  console.log(i)
+}
+
+//167_2
+console.log('Результат задания 167 №2:');
+for (let i = 100; i >= 1; i--) {
+  console.log(i)
+}
+
+//167_3
+console.log('Результат задания 167 №3:');
+for (let i = 2; i <= 100; i += 2) {
+  console.log(i)
+}
+
+//167_4
+console.log('Результат задания 167 №4:');
+let arr4 = [];
+for (let i = 0; i < 10; i++) {
+  arr4.push('x')
+}
+console.log(arr4)
+
+//167_5
+console.log('Результат задания 167 №5:');
+let arr5 = [];
+for (let i = 1; i <= 10; i++) {
+  arr5.push(i);
+}
+console.log(arr5);
+
