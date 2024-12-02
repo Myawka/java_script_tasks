81933f81715f8c937d5e9bb1ec689760a04a01f6
myawka <ssscolllo@gmail.com>
2024-12-02 19:26:17 +0300

    loops

---
+++ loops/tasks 156-166/script.js
@@ -0,0 +1,18 @@
+//156_1
+let str156_1 = "" ;
+for (let i = 1; i <= 9; i++) {
+	for (let j = 1; j <= 3; j++) {
+        str156_1 += i
+	}
+}
+console.log('Результат задания 156 №1: ' + str156_1);
+
+//156_2
+let str156_2 = "" ;
+for (let i = 1; i <= 3; i++) {
+	for (let j = 1; j <= 3; j++) {
+        str156_2 += String(i) + String(j) + ' ';
+	} 
+}
+console.log('Результат задания 156 №2: ' + str156_2);
+
