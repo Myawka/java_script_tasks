a34057c593050ea922754b09104c15031d49acff
myawka <ssscolllo@gmail.com>
2024-12-16 20:22:40 +0300

    174

--- multidimensionality/tasks 173-186/script.js
+++ multidimensionality/tasks 173-186/script.js
@@ -33,3 +33,70 @@ for (let i = 0; i < 3; i++) {
   }
 }
 console.log(arr173_3);
+
+//174_1
+console.log('Результат задания 174 №1:');
+let arr174_1 = [];
+for (let i = 0; i < 3; i++) {
+    arr174_1[i] = []
+	for (let j = 1; j <= 5; j++) {
+		arr174_1[i].push(j);
+	}
+}
+console.log(arr174_1);
+
+//174_2
+console.log('Результат задания 174 №2:');
+let arr174_2 = [];
+for (let i = 0; i < 3; i++) {
+	arr174_2[i] = [];
+	
+	for (let j = 1; j <= 5; j++) {
+		arr174_2[i].push(j);
+	}
+}
+console.log(arr174_2);
+
+//174_3
+console.log('Результат задания 174 №3:');
+let arr174_3 = [];
+for (let i = 0; i < 3; i++) {
+	arr174_3[i] = [];
+	for (let j = 1; j <= 5; j++) {
+		arr174_3[i].push(j);
+	}
+}
+console.log(arr174_3);
+
+//174_4
+console.log('Результат задания 174 №4:');
+let arr174_4 = [];
+for (let i = 0; i < 3; i++) {
+	arr174_4[i] = [];
+	for (let j = 1; j <= 5; j++) {
+		arr174_4[i].push(j);
+	}
+}
+console.log(arr174_4);
+
+//174_5
+console.log('Результат задания 174 №5:');
+let arr174_5 = [];
+for (let i = 0; i < 3; i++) {
+	arr174_5[i] = [];
+	for (let j = 1; j <= 5; j++) {
+		arr174_5[i].push(j);
+	}
+}
+console.log(arr174_5);
+
+//174_6
+console.log('Результат задания 174 №6:');
+let arr174_6 = [];
+for (let i = 0; i < 3; i++) {
+	arr174_6[i] = [];
+	for (let j = 1; j <= 5; j++) {
+		arr174_6[i].push(j);
+	}
+}
+console.log(arr174_6);
