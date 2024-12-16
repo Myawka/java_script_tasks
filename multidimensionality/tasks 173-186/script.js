a612e484bff1e21b537ab0ac1b183873bd4962ed
myawka <ssscolllo@gmail.com>
2024-12-16 20:24:10 +0300

    175

--- multidimensionality/tasks 173-186/script.js
+++ multidimensionality/tasks 173-186/script.js
@@ -100,3 +100,172 @@ for (let i = 0; i < 3; i++) {
 	}
 }
 console.log(arr174_6);
+
+//175_1
+console.log('Результат задания 175 №1:');
+let arr175_1 = [];
+for (let i = 0; i < 4; i++) {
+  arr175_1[i] = [];
+  for (let j = 0; j < 2; j++) {
+    arr175_1[i][j] = i * 2 + j + 1;
+  }
+}
+console.log(arr175_1);
+
+//175_2
+console.log('Результат задания 175 №2:');
+let arr175_2 = [];
+for (let i = 0; i < 4; i++) {
+  arr175_2[i] = [];
+  for (let j = 0; j < 3; j++) {
+    arr175_2[i][j] = (i * 3 + j + 1) * 2;
+  }
+}
+console.log(arr175_2);
+
+//175_3
+console.log('Результат задания 175 №3:');
+let arr175_3 = [];
+let k = 1;
+for (let i = 0; i < 2; i++) {
+  arr175_3[i] = [];
+  for (let j = 0; j < 2; j++) {
+    arr175_3[i][j] = [];
+    for (let l = 0; l < 2; l++) {
+      arr175_3[i][j][l] = k++;
+    }
+  }
+}
+console.log(arr175_3);
+
+//175_4
+console.log('Результат задания 175 №4:');
+let arr175_4 = [];
+let k4 = 1;
+for (let i = 0; i < 3; i++) {
+  arr175_4[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr175_4[i][j] = k4;
+    k4++;
+  }
+}
+console.log(arr175_4);
+
+//175_5
+console.log('Результат задания 175 №5:');
+let arr175_5 = [];
+let k5 = 1;
+for (let i = 0; i < 3; i++) {
+  arr175_5[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr175_5[i][j] = k5;
+    k5++; 
+  }
+}
+console.log(arr175_5);
+
+//175_6
+console.log('Результат задания 175 №6:');
+let arr175_6 = [];
+let k6 = 1;
+for (let i = 0; i < 3; i++) {
+  arr175_6[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr175_6[i][j] = k6;
+    k6++; 
+  }
+}
+console.log(arr175_6);
+
+//175_7
+let arr175_7 = [];
+let k7 = 1; 
+for (let i = 0; i < 3; i++) {
+  arr175_7[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr175_7[i][j] = k7;
+    k7++;
+  }
+}
+console.log(arr175_7);
+
+//175_8
+console.log('Результат задания 175 №8:');
+let arr175_8 = [];
+for (let i = 0, k8 = 1; i < 3; i++) {
+  arr175_8[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr175_8[i][j] = k8;
+    k8++; 
+  }
+}
+console.log(arr175_8);
+
+//175_9
+console.log('Результат задания 175 №9:');
+let arr175_9 = [];
+for (let i = 0, k9 = 1; i < 3; i++) {
+  arr175_9[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr175_9[i][j] = k9;
+    k9++; // Увеличение k
+  }
+}
+console.log(arr175_9);
+
+//175_10
+console.log('Результат задания 175 №10:');
+let arr175_10 = [];
+for (let i = 0, k10 = 1; i < 3; i++) {
+  arr175_10[i] = [];
+  
+  for (let j = 0; j < 3; j++) {
+    arr175_10[i][j] = k10;
+    k10++; // Увеличение k
+  }
+}
+console.log(arr175_10);
+
+//175_11
+console.log('Результат задания 175 №11:');
+let arr175_11 = [];
+for (let i = 0, k11 = 1; i < 3; i++) {
+  arr175_11[i] = [];
+  for (let j = 0; j < 3; j++) {
+    arr175_11[i][j] = k11++;
+  }
+}
+console.log(arr175_11);
+
+//175_12
+console.log('Результат задания 175 №12:');
+let arr175_12 = [];
+let k12 = 1;
+
+for (let i = 0; i < 3; i++) {
+  arr175_12[i] = []; // Исправлено на arr[i]
+  
+  for (let j = 0; j < 3; j++) {
+    arr175_12[i][j] = k12;
+    k12++;
+  }
+}
+console.log(arr175_12);
+
+//175_13
+console.log('Результат задания 175 №13:');
+let arr175_13 = [];
+for (let i = 0, k13 = 1; i < 3; i++) {
+  arr175_13[i] = [];
+  
+  for (let j = 0; j < 3; j++, k13++) { // Добавлено условие для j и увеличение k
+    arr175_13[i][j] = k13;
+  }
+}
+console.log(arr175_13);
